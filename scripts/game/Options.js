8e67a3bc74fec8f1cc3e60969ec3f8a6ef09d83f
Oleksii Filonenko <github@brightone.cloud>
2023-08-07 20:14:44 +0300

    fix(game/options): translate speed preview

--- scripts/game/Options.js
+++ scripts/game/Options.js
@@ -145,7 +145,7 @@ Loader.addSounds([
 		div.innerHTML = "";
 
 		// What's the dialogue?
-		var dialogue = Game.TEXT_SPEED<80 ? "Speak this fast" : "Speak this slow";
+		var dialogue = Game.TEXT_SPEED<80 ? "Так швидко" : "Так повільно";
 
 		// Put in the text
 		var span, chr;
